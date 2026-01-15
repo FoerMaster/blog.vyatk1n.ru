2e741d2c0793c1252606c15d238f27312861cad3
Foer <39239388+FoerMaster@users.noreply.github.com>
2026-01-15 15:16:49 +0500

    First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,25 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
+import ruTranslations from '@/locales/ru.json';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+const locale = 'ru';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang={locale} className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider
+          i18n={{
+            locale,
+            translations: ruTranslations,
+          }}
+        >
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
